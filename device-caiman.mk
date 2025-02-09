ca15afb0011991d117a12b8cb22e39504aaa3a11
Pranav Vashi <neobuddy89@gmail.com>
2025-02-09 22:35:54 +0530

    caimito: Add hide cutout emulations
    Change-Id: I6b3fae23706b749e25d4aecf19c9b882eee0b935
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device-caiman.mk
+++ device-caiman.mk
@@ -134,6 +134,14 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     sensors.dynamic_sensor_hal
 
+# Set support hide display cutout feature
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.support_hide_display_cutout=true
+
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay \
+    AvoidAppsInCutoutOverlay
+
 # VINTF
 DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/vintf/device_framework_matrix_product.xml
